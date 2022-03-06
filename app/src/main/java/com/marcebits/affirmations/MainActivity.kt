d93a53d7c51b3fbb8361984eff8a0e6397fa00d8
Marcelo Carro <16marcelo1bac@gmail.com>
2022-03-06 18:23:29 +0100

    Cómo usar RecyclerView para mostrar una lista desplazable

---
+++ app/src/main/java/com/marcebits/affirmations/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.marcebits.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.marcebits.affirmations.adapter.ItemAdapter
+import com.marcebits.affirmations.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val textView: TextView = findViewById(R.id.textview)
+//        textView.text = Datasource().loadAffirmations().size.toString()
+
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
+    }
+}
---
+++ app/src/main/java/com/marcebits/affirmations/model/Affirmation.kt
@@ -0,0 +1,5 @@
+package com.marcebits.affirmations.model
+
+data class Affirmation(val stringResourceId: Int) {
+
+}
